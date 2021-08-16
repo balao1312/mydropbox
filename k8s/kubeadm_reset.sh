cef4d36818555f27ab9e4b7cc2466d0210e50df1
balao1312 <balao1014@hotmail.com>
2021-08-16 13:37:42 +0800

    kubeadm reset

--- k8s/kubeadm_reset.sh
+++ k8s/kubeadm_reset.sh
@@ -5,10 +5,10 @@ sudo rm -rf /var/lib/cni/
 sudo rm -rf /var/lib/kubelet/*
 sudo rm -rf /run/flannel
 sudo rm -rf /etc/cni/
-ifconfig cni0 down
 sudo systemctl start docker
 rm /home/$USER/.kube
 
+# ifconfig cni0 down
 # brctl delbr cni0
 # ifconfig flannel.1 down
 
