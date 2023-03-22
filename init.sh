d593166931ddfcd4b3b0157fe2e84fcabd73d7f7
balao1312 <balao1014@hotmail.com>
2023-03-22 13:04:42 +0800

    fix a little.

--- init.sh
+++ init.sh
@@ -5,7 +5,5 @@ sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux
 
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
-git clone https://github.com/balao1312/mydropbox.git ~/mydropbox
-
 cp ~/mydropbox/tmuxconf ~/.tmux.conf
 cp ~/mydropbox/vimrc ~/.vimrc
