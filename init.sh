1ab294d1c746f5ced2674ed939bde0716e9f5f9c
balao1312 <balao1014@hotmail.com>
2023-03-22 13:10:10 +0800

    fix a little.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux
+sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping
 
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
