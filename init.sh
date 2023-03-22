c9d0c88f6f1f7e96384eeda09683d818499707f6
balao1312 <balao1014@hotmail.com>
2023-03-22 17:42:25 +0800

    fix a little.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping traceroute
+sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping traceroute tree
 
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
