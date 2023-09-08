2fe9bebc8bbda3ac89ee761e940faa9099248a51
balao1312 <balao1014@hotmail.com>
2023-09-09 01:10:58 +0800

    add two scripts

---
+++ scrcpy_dependency_install.sh
@@ -0,0 +1,4 @@
+sudo apt install ffmpeg libsdl2-2.0-0 adb wget \
+    gcc git pkg-config meson ninja-build libsdl2-dev \
+    libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
+    libswresample-dev libusb-1.0-0 libusb-1.0-0-dev
