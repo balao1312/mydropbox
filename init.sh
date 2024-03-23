067e006f821c2ae7ff9ba2e9b0152114d6d1e2c2
balao1312 <balao1014@hotmail.com>
2024-03-23 12:00:57 +0800

    add package tcpdump.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping traceroute tree openssh-server curl bmon nmap
+sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping traceroute tree openssh-server curl bmon nmap tcpdump
 
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
