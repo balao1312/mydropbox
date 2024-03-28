23a7a93f0cecb93938c11d8fc2c5aea7544dca84
balao1312 <balao1014@hotmail.com>
2024-03-28 17:59:24 +0800

    wireshark install script add privileges for non-root user.

--- wireshark_install.sh
+++ wireshark_install.sh
@@ -1,3 +1,6 @@
 sudo add-apt-repository ppa:wireshark-dev/stable
 sudo apt update
 sudo apt install wireshark
+sudo dpkg-reconfigure wireshark-common
+sudo adduser $USER wireshark
+sudo chmod +x /usr/bin/dumpcap
