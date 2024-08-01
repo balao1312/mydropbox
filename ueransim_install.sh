cab5f64fc036e9cd3acd8d40695ecd5cd77988d1
balao1312 <balao1014@hotmail.com>
2024-08-01 11:08:56 +0800

    add ueransim_install.sh.

---
+++ ueransim_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~
+git clone https://github.com/aligungr/UERANSIM
+sudo apt update
+sudo apt upgrade
+sudo apt install make
+sudo apt install gcc
+sudo apt install g++
+sudo apt install libsctp-dev lksctp-tools
+sudo apt install iproute2
+sudo snap install cmake --classic
+
+cd UERANSIM
+make
