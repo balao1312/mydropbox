6692c5bd42b0f41d044596622b1ec200e8fe0a74
balao1312 <balao1014@hotmail.com>
2024-09-11 15:28:08 +0800

    add sysstat in init.sh.

--- init.sh
+++ init.sh
@@ -3,8 +3,9 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping traceroute tree openssh-server curl bmon nmap tcpdump
-sudo apt-get install -y python3-pip
+sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping \
+    traceroute tree openssh-server curl bmon nmap tcpdump sysstat
+#sudo apt-get install -y python3-pip
 sudo apt-get install -y iproute2 netcat dnsutils
 
 sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
