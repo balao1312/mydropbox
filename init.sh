365d90017c53b26335ae6d5c2edc3ca7c4cb2e8a
balao1312 <balao1014@hotmail.com>
2024-09-11 15:29:25 +0800

    comment out apt upgrade in init.sh.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get upgrade -y
+#sudo apt-get upgrade -y
 
 sudo apt-get install -y htop vim git net-tools iftop iperf3 iperf tmux nload fping \
     traceroute tree openssh-server curl bmon nmap tcpdump sysstat
